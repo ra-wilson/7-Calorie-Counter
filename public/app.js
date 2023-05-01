06b0d0ae4a28285e47104b46bd80c08a32b29da7
ra-wilson <reeceawilson7@gmail.com>
2023-05-01 19:59:34 +0100

    more features

--- public/app.js
+++ public/app.js
@@ -1,82 +1,129 @@
+// // Import and configure Firebase
+// import firebase from 'https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js';
+// import 'https://www.gstatic.com/firebasejs/8.10.0/firebase-auth.js';
+// import 'https://www.gstatic.com/firebasejs/8.10.0/firebase-firestore.js';
+
+// Replace the configuration object with your own Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyBRj9v5HWIXL_QEAMv2Qrx645mkEphe6Rw",
+  authDomain: "calorie-counter-b8b8c.firebaseapp.com",
+  projectId: "calorie-counter-b8b8c",
+  storageBucket: "calorie-counter-b8b8c.appspot.com",
+  messagingSenderId: "726787238895",
+  appId: "1:726787238895:web:b2e26d7cc4fe3515dbe41a",
+  measurementId: "G-FL5ET271Q4",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+
 // Variables to store calorie data
 let calorieGoal = 0;
 let dailyCalories = {
-    breakfast: 0,
-    lunch: 0,
-    dinner: 0,
-    snacks: 0,
+  breakfast: 0,
+  lunch: 0,
+  dinner: 0,
+  snacks: 0,
 };
 
 // Set calorie goal
 $("#calorie-goal-form").submit(function (event) {
-    event.preventDefault();
-    calorieGoal = parseInt($("#calorie-goal").val());
-    $("#daily-goal").text(calorieGoal);
-    $("#submit-goal").text('Edit goal');
+  event.preventDefault();
+  calorieGoal = parseInt($("#calorie-goal").val());
+  $("#daily-goal").text(calorieGoal);
+  $("#submit-goal").text("Edit goal");
 });
-
 
 // Add meal input
-$(".meal-form").on("submit", function (e) {
+$(".meal-form").on("submit", async function (e) {
   e.preventDefault();
+
+  if (!firebase.auth().currentUser) {
+    alert("Please log in to add meals.");
+    return;
+  }
+
+  const userId = firebase.auth().currentUser.uid;
   const mealType = $(this).data("meal-type");
-  const foodItems = $(this).find("input").val().split(",").map(item => item.trim());
+  const foodItems = $(this)
+    .find("input")
+    .val()
+    .split(",")
+    .map((item) => item.trim());
   const foodItemsContainer = $(this).find(".food-items-container");
 
-  const caloriesPromises = foodItems.map(item => getCaloriesFromAPI(item));
+  const caloriesPromises = foodItems.map((item) => getCaloriesFromAPI(item));
 
   Promise.all(caloriesPromises)
-      .then(caloriesArray => {
-          const totalCalories = caloriesArray.reduce((a, b) => a + b, 0);
-          dailyCalories[mealType] += totalCalories;
+    .then(async (caloriesArray) => {
+      const totalCalories = caloriesArray.reduce((a, b) => a + b, 0);
+      dailyCalories[mealType] += totalCalories;
+      updateCalorieSummary();
+      saveDailyCalories();
+
+      // Save meal data to Firestore
+      try {
+        await db
+          .collection("users")
+          .doc(userId)
+          .collection("meals")
+          .add({
+            date: firebase.firestore.Timestamp.fromDate(new Date()),
+            mealType,
+            foodItems,
+            calories: totalCalories,
+          });
+      } catch (error) {
+        console.error("Error adding meal data to Firestore:", error);
+      }
+
+      // Add the input food items to the container
+      foodItems.forEach((foodItem, index) => {
+        const foodItemElement = $("<p>").text(
+          `${foodItem} (${caloriesArray[index]} calories)`
+        );
+        const deleteIcon = $("<i>").addClass("fas fa-trash-alt delete-icon");
+        foodItemElement.append(deleteIcon);
+        foodItemsContainer.append(foodItemElement);
+
+        deleteIcon.on("click", () => {
+          dailyCalories[mealType] -= caloriesArray[index];
           updateCalorieSummary();
-
-          // Add the input food items to the container
-          foodItems.forEach((foodItem, index) => {
-              const foodItemElement = $("<p>").text(`${foodItem} (${caloriesArray[index]} calories)`);
-              const deleteIcon = $('<i>').addClass('fas fa-trash-alt delete-icon');
-              foodItemElement.append(deleteIcon);
-              foodItemsContainer.append(foodItemElement);
-          
-
-          deleteIcon.on("click", () => {
-            dailyCalories[mealType] -= caloriesArray[index];
-            updateCalorieSummary();
-            foodItemElement.remove();
-          })
+          foodItemElement.remove();
         });
-
-          // Clear the input field
-          $(this).find("input").val("");
-      })
-      .catch(error => {
-          console.error("Error fetching calorie data:", error);
       });
+
+      // Clear the input field
+      $(this).find("input").val("");
+    })
+    .catch((error) => {
+      console.error("Error fetching calorie data:", error);
+    });
 });
-
-
 
 // Function to make AJAX call to Ninja Nutrition API (replace with actual API call)
 // Function to make AJAX call to Ninja Nutrition API
- function getCaloriesFromAPI(query) {
+function getCaloriesFromAPI(query) {
   return new Promise((resolve, reject) => {
-      $.ajax({
-          method: 'GET',
-          url: 'https://api.api-ninjas.com/v1/nutrition?query=' + query,
-          headers: { 'X-Api-Key': 'i5PsWsLfdY890euq8xNxCg==gDrteQ1qs1O03zXG'},
-          contentType: 'application/json',
-          success: function(result) {
-              console.log(result); // Log the API response
-              const calories = Math.round(result[0].calories);
-              resolve(calories);
-          },
-          error: function(jqXHR) {
-              console.error('Error: ', jqXHR.responseText);
-              reject(jqXHR);
-          }
-      });
+    $.ajax({
+      method: "GET",
+      url: "https://api.api-ninjas.com/v1/nutrition?query=" + query,
+      headers: { "X-Api-Key": "i5PsWsLfdY890euq8xNxCg==gDrteQ1qs1O03zXG" },
+      contentType: "application/json",
+      success: function (result) {
+        console.log(result); // Log the API response
+        const calories = Math.round(result[0].calories);
+        resolve(calories);
+      },
+      error: function (jqXHR) {
+        console.error("Error: ", jqXHR.responseText);
+        reject(jqXHR);
+      },
+    });
   });
-} 
+}
 
 // function getCaloriesFromAPI(query) {
 //   return new Promise((resolve, reject) => {
@@ -98,93 +145,234 @@ $(".meal-form").on("submit", function (e) {
 //   });
 // }
 
-
 $("#login-form").submit(function (event) {
   event.preventDefault();
-  const username = $("#login-username").val();
+  const email = $("#login-username").val();
   const password = $("#login-password").val();
 
-  $.ajax({
-    method: 'POST',
-    url: '/login',
-    data: JSON.stringify({ username, password }),
-    contentType: 'application/json',
-    success: function (result) {
+  auth
+    .signInWithEmailAndPassword(email, password)
+    .then((result) => {
       console.log(result);
-      $("#auth-container").hide();
-      $("#main-container").show();
-    },
-    error: function (jqXHR) {
-      console.error('Error: ', jqXHR.responseText);
-    },
-  });
+      loginUser(); // Call loginUser() after a successful login
+    })
+    .catch((error) => {
+      console.error("Error: ", error.message);
+    });
 });
 
-
+// Update the registration function
 $("#register-form").submit(function (event) {
   event.preventDefault();
-  const username = $("#register-username").val();
+  const email = $("#register-username").val();
   const password = $("#register-password").val();
 
-  $.ajax({
-    method: 'POST',
-    url: '/register',
-    data: JSON.stringify({ username, password }),
-    contentType: 'application/json',
-    success: function (result) {
+  auth
+    .createUserWithEmailAndPassword(email, password)
+    .then((result) => {
       console.log(result);
-      alert('Registration successful! You can now log in.');
-    },
-    error: function (jqXHR) {
-      console.error('Error: ', jqXHR.responseText);
-    },
-  });
+      alert("Registration successful! You can now log in.");
+    })
+    .catch((error) => {
+      console.error("Error: ", error.message);
+    });
+});
+
+$("#show-login").on("click", function (e) {
+  e.preventDefault();
+  $("#register-container").hide();
+  $("#login-container").show();
+});
+
+$("#show-register").on("click", function (e) {
+  e.preventDefault();
+  $("#login-container").hide();
+  $("#register-container").show();
 });
 
 
-
-
-
-// Function to update calorie summary
-function updateCalorieSummary() {
-    const totalCalories = Object.values(dailyCalories).reduce((a, b) => a + b, 0);
-    // Update the display with the totalCalories and calorieGoal
-}
-
-
-// Initialize datepicker
 $("#datepicker").datepicker({
-  onSelect: function (dateText, inst) {
-      // Fetch calorie data for the selected date and update the display
-      // You'll need to implement the logic to fetch data from your database
+  onSelect: async function (dateText, inst) {
+    const selectedDate = dateText.split('/').reverse().join('-');
+    const meals = await fetchMealDataForDate(selectedDate);
+    updateMealDisplay(meals);
+    updateCalorieSummary();
   },
 });
 
-// Function to update calorie summary
-// function updateCalorieSummary() {
-//   const totalCalories = Object.values(dailyCalories).reduce((a, b) => a + b, 0);
-//   const calorieStatus = totalCalories > calorieGoal ? "beyond" : "within";
-//   const calorieDifference = Math.abs(totalCalories - calorieGoal);
-  
-//   const summaryText = `Today's total calories: ${totalCalories} (Goal: ${calorieGoal}). You are ${calorieDifference} calories ${calorieStatus} your goal.`;
-//   $("#calorie-summary-text").text(summaryText);
-// }
 
+async function fetchMealDataForDate(date) {
+  const userId = firebase.auth().currentUser.uid;
+  const userDocRef = firebase.firestore().collection("users").doc(userId);
+  const querySnapshot = await userDocRef.collection("meals").where("date", "==", firebase.firestore.Timestamp.fromDate(new Date(date))).get();
+
+  if (!querySnapshot.empty) {
+    let meals = {
+      breakfast: [],
+      lunch: [],
+      dinner: [],
+      snacks: [],
+    };
+
+    querySnapshot.forEach((doc) => {
+      const data = doc.data();
+      meals[data.mealType].push({
+        id: doc.id,
+        foodItems: data.foodItems,
+        calories: data.calories,
+      });
+    });
+
+    return meals;
+  } else {
+    return {
+      breakfast: [],
+      lunch: [],
+      dinner: [],
+      snacks: [],
+    };
+  }
+}
+
+
+function saveDailyCalories() {
+  const userId = firebase.auth().currentUser.uid;
+  const date = dailyCalories.date;
+  const userDocRef = firebase.firestore().collection("users").doc(userId);
+  userDocRef.collection("calories").doc(date).set(dailyCalories);
+}
+
+
+function resetDailyCalories() {
+  dailyCalories = {
+    breakfast: 0,
+    lunch: 0,
+    dinner: 0,
+    snacks: 0,
+  };
+}
+
+// Function to update calorie summary
 
 function updateCalorieSummary() {
-    const totalCalories = Object.values(dailyCalories).reduce((a, b) => a + b, 0);
-    
-    // Update the calorie goal and total calories display
-    $("#total-calories").text(totalCalories);
-    $("#calorie-goal").text(calorieGoal);
-  
-    // Change the color based on the goal status
-    if (totalCalories < calorieGoal) {
-      $("#total-calories").removeClass('goal-met').addClass('goal-not-met');    
-    } else {
-      $("#total-calories").removeClass('goal-not-met').addClass('goal-met');
-    }
+  const totalCalories = Object.values(dailyCalories)
+    .filter(value => typeof value === 'number')
+    .reduce((a, b) => a + b, 0);
+
+  // Update the calorie goal and total calories display
+  $("#total-calories").text(totalCalories);
+  $("#calorie-goal").text(calorieGoal);
+
+  // Get the user's weight goal selection
+  const weightGoal = $('input[name="weight-goal"]:checked').val();
+
+  // Change the color based on the goal status and user selection
+  if (weightGoal === 'deficit' && totalCalories < calorieGoal) {
+    $("#total-calories").removeClass('goal-not-met').addClass('goal-met');
+  } else if (weightGoal === 'surplus' && (totalCalories >= calorieGoal - 200 && totalCalories <= calorieGoal + 300)) {
+    $("#total-calories").removeClass('goal-not-met').addClass('goal-met');
+  } else {
+    $("#total-calories").removeClass('goal-met').addClass('goal-not-met');
   }
+}
+
+function updateMealDisplay(meals) {
+  // Clear the current meal display
+  $(".food-items-container").empty();
+
+  // Add the fetched meal data to the display
+  for (const mealType in meals) {
+    const mealForm = $(`.meal-form[data-meal-type="${mealType}"]`);
+    const foodItemsContainer = mealForm.find(".food-items-container");
+
+    meals[mealType].forEach((meal) => {
+      meal.foodItems.forEach((foodItem, index) => {
+        const foodItemElement = $("<p>").text(
+          `${foodItem} (${meal.calories[index]} calories)`
+        );
+        const deleteIcon = $("<i>").addClass("fas fa-trash-alt delete-icon");
+        foodItemElement.append(deleteIcon);
+        foodItemsContainer.append(foodItemElement);
+
+        deleteIcon.on("click", () => {
+          // Update dailyCalories, remove the food item, and update the UI
+        });
+      });
+    });
+  }
+}
+
+
+
+
+const SESSION_EXPIRY_TIME = 10 * 60 * 1000; // 10 minutes in milliseconds
+
+// Check if the user is already logged in when the page is loaded
+$(document).ready(function() {
+  const loginState = localStorage.getItem('loggedIn');
+  const lastActivity = localStorage.getItem('lastActivity');
+  const currentTime = new Date().getTime();
+
+  if (loginState === 'true' && (currentTime - lastActivity) < SESSION_EXPIRY_TIME) {
+    showMainContainer();
+    updateActivity();
+  } else {
+    localStorage.setItem('loggedIn', 'false');
+    localStorage.removeItem('lastActivity');
+    showAuthContainer();
+  }
+});
+
+// Update the user's last activity timestamp
+function updateActivity() {
+  const currentTime = new Date().getTime();
+  localStorage.setItem('lastActivity', currentTime);
+}
+
+// Log the user in and update the session
+function loginUser() {
+  localStorage.setItem('loggedIn', 'true');
+  updateActivity();
+  showMainContainer();
+}
+
+// Log the user out and clear the session
+
+function logoutUser() {
+  auth
+    .signOut()
+    .then(() => {
+      localStorage.setItem('loggedIn', 'false');
+      localStorage.removeItem('lastActivity');
+      showAuthContainer();
+      alert("You have been logged out.");
+    })
+    .catch((error) => {
+      console.error("Error: ", error.message);
+      alert("Failed to log out. Please try again.");
+    });
+}
+
+
+// Set up event listeners for login and logout buttons
+$(document).on('click', '#login-button', loginUser);
+$(document).on('click', '#logout-button', logoutUser);
+
+// Update the last activity timestamp whenever the user interacts with the page
+$(document).on('mousemove keydown click', updateActivity);
+
+// Show and hide the auth container and main container
+function showAuthContainer() {
+  $('#auth-container').show();
+  $('#main-container').hide();
+  $('#logout-button').hide(); // Hide the logout button when showing the auth container
+}
+
+function showMainContainer() {
+  $('#auth-container').hide();
+  $('#main-container').show();
+  $('#logout-button').show(); // Show the logout button when showing the main container
+}
 
 
 
---
+++ public/firebase.js
@@ -0,0 +1,22 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBRj9v5HWIXL_QEAMv2Qrx645mkEphe6Rw",
+//   authDomain: "calorie-counter-b8b8c.firebaseapp.com",
+//   projectId: "calorie-counter-b8b8c",
+//   storageBucket: "calorie-counter-b8b8c.appspot.com",
+//   messagingSenderId: "726787238895",
+//   appId: "1:726787238895:web:b2e26d7cc4fe3515dbe41a",
+//   measurementId: "G-FL5ET271Q4"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
